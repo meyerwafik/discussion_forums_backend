add1dc397cc90457cc2b391c8225537554659285
Meyer Wafik <meyerwafik@gmail.com>
2021-12-06 17:21:35 +0200

    add and delete

--- app/app.js
+++ app/app.js
@@ -10,14 +10,21 @@ discussions=[]
 comments=[]
 discussion={}
 course={}
+student={}
 discussion={}
 commentObj={}
 
 const getCourses=   async (user)=>
 {
+  if(user.role==="Student"){
   courses = await database.users.findOne({where:{id:user.id},include:['courses']});
-  return coursesArray;  
+  }
+  else{
+    courses=await database.courses.findAll();
+  }
+  return courses;  
 }
+
 
 const getDiscussions=   async (courseId)=>
 {
@@ -34,12 +41,55 @@ title,discDescription,userId:user.id,courseId
  return discussion
 }
 
+const createCourse=async ({courseName,courseDescription})=>{
+  console.log(courseName,courseDescription)
+   course=await database.courses.create({
+    courseName,courseDescription
+  })
+   return course
+  }
+
 const createComment=async({content},discussionId,user)=>{
    comment=await database.comments.create({
       userId:user.id,content,discussionId
     })
     return comment
    }
+
+
+   const deleteCourse=async(courseId)=>{
+     let success=false;
+     let course=await database.courses.findByPk(courseId)
+     if(!course){
+       return success
+     }
+     else{
+     course.destroy();
+     success=true
+     return success
+     }
+   }
+
+   const createStudent=async( studentName, email, password,userRole)=>{
+     let  encryptedPassword = await bcrypt.hash(password, 10);
+    student=await database.users.create({
+      studentName,email,pwHash:encryptedPassword,userRole:userRole
+     })
+     return student;
+    }
+
+   const deleteStudent=async(userId)=>{
+    let success=false;
+    let user=await database.users.findByPk(userId)
+    if(!user){
+      return success
+    }
+    else{
+      user.destroy();
+    success=true
+    return success
+    }
+  }
    
 
 const getComments=  async (discussionId,userToken)=>
@@ -76,9 +126,12 @@ u = await database.users.findOne({ where: { email: email} })
     );
     
     u.token = token;
- 
   }
+ else{
+   u=false;
+ }
+  
   return u
 } 
 
-module.exports={getCourses,getDiscussions,createDiscussion,getComments,createComment,login,getUser}
+module.exports={getCourses,getDiscussions,createDiscussion,getComments,createComment,login,getUser,deleteCourse,deleteStudent,createStudent,createCourse}
